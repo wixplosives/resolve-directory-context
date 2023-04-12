ada44a1c2d2c4c7907754d6bfc7ad11c474d2b9e
Avi Vahl <avi@vahl.co.il>
2023-04-12 17:55:47 +0300

    chore(deps): minimatch@9.0.0 and regenerate lock file

--- src/workspaces.ts
+++ src/workspaces.ts
@@ -1,4 +1,4 @@
-import minimatch from 'minimatch';
+import { minimatch } from 'minimatch';
 import type { PackageJson } from 'type-fest';
 import { isString, isPlainObject } from './language-helpers';
 import { INpmPackage, PACKAGE_JSON } from './npm-package';
